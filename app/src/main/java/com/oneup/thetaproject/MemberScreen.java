42ded89ff3cfa5abdef23f463d6b78bee8be1d96
johntaaa <birdmcfly1@gmail.com>
2017-08-04 16:23:34 -0400

    Finished object for listview; finished some of the primary members screen

---
+++ app/src/main/java/com/oneup/thetaproject/MemberScreen.java
@@ -0,0 +1,13 @@
+package com.oneup.thetaproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemberScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_screen);
+    }
+}
