ed050582faf0c81049efd4d10361213614843434
khincce <ksegenchuk@wpi.edu>
2017-08-04 14:00:46 -0400

    made tabs

--- app/src/main/java/com/oneup/thetaproject/MainActivity.java
+++ app/src/main/java/com/oneup/thetaproject/MainActivity.java
@@ -1,13 +1,54 @@
 package com.oneup.thetaproject;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentCalendar fragmentC;
+    private FragmentGroups fragmentG;
+    private FragmentNotes fragmentN;
+    private FragmentSettings fragmentS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Groups"));
+        tabLayout.addTab(tabLayout.newTab().setText("Calendar"));
+        tabLayout.addTab(tabLayout.newTab().setText("Notes"));
+        tabLayout.addTab(tabLayout.newTab().setText("Setting"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final android.support.v4.view.ViewPager viewPager = (android.support.v4.view.ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // while unselect the Tab
+
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // while reselect the Tab
+
+            }
+        });
+
     }
 }
